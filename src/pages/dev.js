2694be1e800546e19f8a9534f5249f23b78fffee
Eric Fortney <efortney28@gmail.com>
2020-12-07 22:12:22 -0600

    responsive testing

--- src/pages/dev.js
+++ src/pages/dev.js
@@ -1,9 +1,10 @@
 import React from "react"
 import { Link } from "gatsby"
 
-
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+
+import "../styles/dev.css"
 
 const Dev = ({data}) => {
   const posts = data.allContentfulDevProject.edges
@@ -11,7 +12,27 @@ const Dev = ({data}) => {
   return (
     <Layout>
       <SEO title="Development" />
-      <section className="info-container">
+      <section className="container">
+        <article className="dev-info">
+          <h2 className="page-title">Development</h2>
+          <p className="page-description">Ever since I was little I've been fascinated with anything and everything to do with the internet. I remember getting a "How to Build Websites: For Dummies" in the early 2000's. Ever since then, I've always wanted to build websites. Now that I'm older, I taught myself how to build anything I or anybody else could want.</p>
+        </article>
+        <section className="dev-container">
+          {posts.map(({ node: post }) => (
+            <a target="_blank" className="" href={post.url}>
+              <article className="dev-post">
+                <img className="dev-img" src={post.screenshot.file.url} />
+                <section className="dev-content">
+                  <h3 className="dev-title">{post.projectTitle}</h3>
+                  <div className="dev-description" dangerouslySetInnerHTML={{__html: post.description.childContentfulRichText.html}}></div>
+                  <p className="dev-link"><a className="link dev-link" target="__blank" href={post.githubUrl}>GitHub Repo</a></p>
+                </section>
+              </article>
+            </a>
+          ))}
+        </section>
+      </section>
+      {/* <section className="info-container">
         <h2 className="page-title">Development Projects</h2>
         <p className="page-description">Ever since I was little I've been fascinated with anything and everything to do with the internet. I remember getting a "How to Build Websites: For Dummies" in the early 2000's. Ever since then, I've always wanted to build websites. Now that I'm older, I taught myself how to build anything I or anybody else could want.</p>
       </section>
@@ -26,7 +47,7 @@ const Dev = ({data}) => {
             </article>
           </a>
         ))}
-      </section>
+      </section> */}
     </Layout>
   )
 }
